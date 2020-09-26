3eeedcd4e7628777380080b187c6264b10a92499
mameli <mameli93@gmail.com>
2020-09-26 20:08:42 +0200

    unicafe step1

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,30 +1,32 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const App = (props) => {
-  const [value, setValue] = useState(10)
+const Button = (props) => (
+  <button onClick={props.handleClick}> 
+    {props.text}
+  </button>
+)
 
-  const setToValue = (newValue) => {
-    setValue(newValue)
-  }
+const App = () => {
+  // save clicks of each button to own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
   return (
     <div>
-      {value}
-      <button onClick={() => setToValue(1000)}>
-        thousand
-      </button>
-      <button onClick={() => setToValue(0)}>
-        reset
-      </button>
-      <button onClick={() => setToValue(value + 1)}>
-        increment
-      </button>
+      <h1>give feedback</h1>
+      <Button handleClick={() => setGood(good + 1)} text = "Good"/>
+      <Button handleClick={() => setNeutral(neutral + 1)} text = "Neutral"/>
+      <Button handleClick={() => setBad(bad + 1)} text = "Bad"/>
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
     </div>
   )
 }
 
-ReactDOM.render(
-  <App />,
+ReactDOM.render(<App />, 
   document.getElementById('root')
 )
