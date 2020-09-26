f2f4a49586982e429d9f2fc9cfa750a13bb02834
mameli <mameli93@gmail.com>
2020-09-26 17:42:33 +0200

    Handling arrays

--- part1/src/index.js
+++ part1/src/index.js
@@ -2,26 +2,28 @@ import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
 const App = (props) => {
-  const [clicks, setClicks] = useState(
-    {
-      left: 0,
-      right: 0
-    }
-  )
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
 
-  const handleLeftClick = () =>
-  setClicks({ ...clicks, left: clicks.left + 1 })
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+  }
 
-const handleRightClick = () =>
-  setClicks({ ...clicks, right: clicks.right + 1 })
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+  }
 
   return (
     <div>
       <div>
-        {clicks.left}
+        {left}
         <button onClick={handleLeftClick}>left</button>
         <button onClick={handleRightClick}>right</button>
-        {clicks.right}
+        {right}
+        <p>{allClicks.join(' ')}</p>
       </div>
     </div>
   )
