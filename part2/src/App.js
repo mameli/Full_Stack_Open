5e26280e9d18fa15c13dbd7e1b6ad2585f6c4a53
mameli <mameli93@gmail.com>
2020-09-28 17:06:50 +0200

    Course information step8

--- part2/src/App.js
+++ part2/src/App.js
@@ -1,17 +1,52 @@
 import React from 'react'
-import Note from './components/Note'
 
-const App = ({ notes }) => {
+const Course = ({ course }) => {
   return (
     <div>
-      <h1>Notes</h1>
-      <ul>
-        {notes.map((note) => 
-          <Note key={note.id} note={note} />
-        )}
-      </ul>
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+    </div>
+  );
+};
+
+const Header = ({ name }) => <h1>{name}</h1>;
+
+const Content = ({ parts }) => {
+
+  const total = parts.reduce((sum, part) => sum + part.exercises, 0);
+  
+  return (
+    <div>
+      {parts.map(part => <p key={part.id}>{part.name} {part.exercises}</p>)}
+      {total}
     </div>
   )
+}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
 }
 
 export default App
