8edde7c7558b101df1dfbc7668c474bfc6995961
mameli <mameli93@gmail.com>
2020-10-09 18:01:08 +0200

    Axios and promises

--- part2/src/App.js
+++ part2/src/App.js
@@ -1,85 +1,33 @@
-import React, { useState } from 'react'
-
-const Person = ({ person }) => {
-  return (
-    <li>{person.name} {person.number}</li>
-  )
-}
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import Note from './components/Note'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
-  const [nameFilter, setNameFilter] = useState('')
+  const [notes, setNotes] = useState([])
+  // const [newNote, setNewNote] = useState('')
+  // const [showAll, setShowAll] = useState(true)
+
+  const hook = () => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        console.log('promise fulfilled')
+        setNotes(response.data)
+      })
+  }
+
+  useEffect(hook, [])
+  console.log('render', notes.length, 'notes')
   
-  const checkDuplicates = (name) => {
-    const filteredList = persons.filter(p => p.name === newName)
-    if (filteredList.length > 0) {
-      return true
-    }
-    return false
-  }
-
-  const addName = (event) => {
-    event.preventDefault()
-    const person = {
-      name: newName,
-      number: newNumber
-    }
-    if (!checkDuplicates(newName)) {
-      setPersons(persons.concat(person))
-    } else {
-      window.alert(`${newName} is already added to phonebook`)
-    }
-    setNewName('')
-    setNewNumber('')
-  }
-
-  const handleNewName = (event) => {
-    setNewName(event.target.value)
-  }
-
-  const handleNewNumber = (event) => {
-    setNewNumber(event.target.value)
-  }
-
-  const handleNameFilter = (event) => {
-    setNameFilter(event.target.value)
-  }
-
-  return (
-    <div>
-      <h2>Phonebook</h2>
-      <form>
-        <div>
-          filter shown with <input value={nameFilter} onChange={handleNameFilter}/>
-        </div>
-      </form>
-      <h2>Add New</h2>
-      <form onSubmit={addName}>
-        <div>
-          name: <input value={newName} onChange={handleNewName} />
-        </div>
-        <div>
-          number: <input value={newNumber} onChange={handleNewNumber}/>
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      <ul>
-        {persons.filter(p => p.name.toLowerCase().includes(nameFilter))
-                .map(p =>
-                <Person key={p.name} person={p} />
-        )}
-      </ul>
-    </div>
+  return(<div>
+    <ul>
+      {notes.map(n =>
+              <Note key={n.id} note={n} />
+      )}
+    </ul>
+  </div>
+    
   )
 }
 
