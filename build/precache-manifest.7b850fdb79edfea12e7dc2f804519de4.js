3d535011cd02f5c0f57495fbe5883a74c55fbac2
mameli <mameli93@gmail.com>
2020-10-22 01:06:10 +0200

    fe build

---
+++ build/precache-manifest.7b850fdb79edfea12e7dc2f804519de4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34750d8b59827e9c939b06aa1aa0901c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7478b6118aec8e466980",
+    "url": "/static/js/2.b2e28815.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b2e28815.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6525cb5068c4d1ed6fd",
+    "url": "/static/js/main.e9894df0.chunk.js"
+  },
+  {
+    "revision": "4b4367ca4fbadb01e2d9",
+    "url": "/static/js/runtime-main.e5a25448.js"
+  }
+]);
