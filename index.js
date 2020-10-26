1eb90f2d543dd277232351fec8a1b33ed5a3fa60
mameli <mameli93@gmail.com>
2020-10-26 22:11:29 +0100

    refactor mongo

--- index.js
+++ index.js
@@ -11,38 +11,8 @@ app.use(express.static('build'))
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-let persons = [
-    {
-        "name": "Arto Hellas",
-        "number": "040-123456",
-        "id": 1
-    },
-    {
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523",
-        "id": 2
-    },
-    {
-        "name": "Dan Abramov",
-        "number": "12-43-234345",
-        "id": 3
-    },
-    {
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122",
-        "id": 4
-    },
-    {
-        "name": "asdf",
-        "number": "444",
-        "id": 5
-    },
-    {
-        "name": "12341234",
-        "number": "3123412",
-        "id": 6
-    }
-]
+require('dotenv').config()
+const Person = require('./models/person')
 
 app.get('/', (request, response) => {
     response.send('<h1>Phonebook!</h1>')
@@ -57,34 +27,30 @@ app.get('/info', (request, response) => {
 })
 
 app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.filter(p => p.id === id)
-
-    if (persons) {
-        response.json(person)
-    } else {
-        response.status(404).end()
-    }
+    Person.findById(request.params.id).then(p => {
+        if (p) {
+            response.json(p)
+        }else {
+            response.status(404).end()
+        }
+    })
+    .catch(error => {
+        console.log(error)
+        response.status(500).end()
+    })
 })
 
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    Person.find({}).then(p => {
+        response.json(p)
+    })
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(p => p.id !== id)
-
-    response.status(204).end()
+    Person.findByIdAndDelete(request.params.id).then(p => {
+        response.json(p)
+    })
 })
-
-const generateId = () => {
-    return Math.floor(Math.random() * Math.floor(100))
-}
-
-const checkDuplicates = (newName) => {
-    return persons.filter(p => p.name === newName).length > 0
-}
 
 app.post('/api/persons/', (request, response) => {
     const body = request.body
@@ -95,26 +61,19 @@ app.post('/api/persons/', (request, response) => {
         })
     }
 
-    if (checkDuplicates(body.name)) {
-        return response.status(400).json({
-            error: 'name must be unique'
-        })
-    }
-
-    const newPerson = {
-        id: generateId(),
+    const newPerson = new Person({
         name: body.name,
-        number: body.number
-    }
+        number: body.number,
+    })
 
-    persons = persons.concat(newPerson)
-
-    response.json(newPerson)
+    newPerson.save().then(savedPerson => {
+        response.json(savedPerson)
+    })
 })
 
 
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
