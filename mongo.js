ba0c7201e98b1af9a21bc904b63356c17436b884
mameli <mameli93@gmail.com>
2020-11-08 16:50:23 +0100

    eslint

--- mongo.js
+++ mongo.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
-    console.log('Please provide the password as an argument: node mongo.js <password>')
-    process.exit(1)
+	console.log('Please provide the password as an argument: node mongo.js <password>')
+	process.exit(1)
 }
 
 const password = process.argv[2]
@@ -13,33 +13,33 @@ const url =
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: Number,
+	name: String,
+	number: Number,
 })
 
 const Person = mongoose.model('Persons', personSchema)
 
 if (process.argv.length === 5) {
-    console.log(process.argv[3], process.argv[4]);
+	console.log(process.argv[3], process.argv[4])
 
-    const person = new Person({
-        name: process.argv[3],
-        number: process.argv[4],
-    })
+	const person = new Person({
+		name: process.argv[3],
+		number: process.argv[4],
+	})
 
-    person.save().then(result => {
-        console.log('Person saved!')
-        mongoose.connection.close()
-    })
+	person.save().then(() => {
+		console.log('Person saved!')
+		mongoose.connection.close()
+	})
 }
 
 if (process.argv.length === 3) {
-    Person.find({}).then(result => {
-        result.forEach(p => {
-            console.log(p)
-        })
-        mongoose.connection.close()
-    })
+	Person.find({}).then(result => {
+		result.forEach(p => {
+			console.log(p)
+		})
+		mongoose.connection.close()
+	})
 }
 
 // Note.find({}).then(result => {
