e4cdc1b44e7e2badc8a3fb8b8b27c4de854e4903
mameli <mameli93@gmail.com>
2020-11-22 23:32:09 +0100

    fix express-async-errors import position

--- utils/config.js
+++ utils/config.js
@@ -1,10 +1,10 @@
 require('dotenv').config()
 
 const PORT = process.env.PORT
-let MONGODB_URI = process.env.MONGODB_URI
+let MONGODB_URI = process.env.MONGODB_URI_NOTE
 
 if (process.env.NODE_ENV === 'test') {
-	MONGODB_URI = process.env.TEST_MONGODB_URI
+	MONGODB_URI = process.env.TEST_MONGODB_URI_NOTE
 }
 
 module.exports = {
