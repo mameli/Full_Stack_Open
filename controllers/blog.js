4d7ebac1d3a04495b84d284036f543fd5e991098
mameli <mameli93@gmail.com>
2020-11-28 19:24:47 +0100

    added blog users

--- controllers/blog.js
+++ controllers/blog.js
@@ -1,8 +1,10 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
 	const blogs = await Blog.find({})
+		.find({}).populate('user', { username: 1, name: 1 })
 	response.json(blogs)
 })
 
@@ -16,9 +18,16 @@ blogsRouter.get('/:id', async (request, response) => {
 })
 
 blogsRouter.post('/', async (request, response) => {
-	const blog = new Blog(request.body)
+	const user = await User.findById(request.body.userId)
+	const body = request.body
+	body['user'] = user._id
+	const blog = new Blog(body)
 
 	const newBlog = await blog.save()
+
+	user.blogs = user.blogs.concat(newBlog._id)
+	await user.save()
+
 	response.status(201).json(newBlog)
 })
 
--- utils/config.js
+++ utils/config.js
@@ -1,10 +1,10 @@
 require('dotenv').config()
 
 const PORT = process.env.PORT
-let MONGODB_URI = process.env.MONGODB_URI
+let MONGODB_URI = process.env.MONGODB_URI_BLOG
 
 if (process.env.NODE_ENV === 'test') {
-	MONGODB_URI = process.env.TEST_MONGODB_URI
+	MONGODB_URI = process.env.TEST_MONGODB_URI_BLOG
 }
 
 module.exports = {
