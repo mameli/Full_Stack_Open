55ae6533ebcac7ffdfd92f7452b5f2fdfedab52b
mameli <mameli93@gmail.com>
2020-12-05 22:25:06 +0100

    login visible

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,27 +1,24 @@
 import React from "react";
 
-const LoginForm = ({
-  handleSubmit,
-  handleUsernameChange,
-  handlePasswordChange,
-  username,
-  password,
-}) => {
+const LoginForm = (props) => {
   return (
     <div>
       <h2>Login</h2>
-
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={props.handleSubmit}>
         <div>
           username
-          <input value={username} onChange={handleUsernameChange} />
+          <input
+            value={props.username}
+            onChange={props.handleChange}
+            name="username"
+          />
         </div>
         <div>
           password
           <input
             type="password"
-            value={password}
-            onChange={handlePasswordChange}
+            value={props.password}
+            onChange={props.handlePasswordChange}
           />
         </div>
         <button type="submit">login</button>
