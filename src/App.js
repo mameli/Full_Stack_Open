10e67b369a25657c76b2b95d584bdca1d91ac41b
mameli <mameli93@gmail.com>
2020-12-10 00:35:34 +0100

    update likes

--- src/App.js
+++ src/App.js
@@ -100,7 +100,7 @@ const App = () => {
       </div>
       {blogForm()}
       {blogs.map((blog) => (
-        <Blog key={blog.id} blog={blog} />
+        <Blog key={blog.id} blog={blog} updateBlogs={setBlogs} />
       ))}
     </div>
   );
--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,56 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from "react";
+import blogService from "../services/blogs";
 
-export default Blog
+const Blog = ({ blog, updateBlogs }) => {
+  const [isHidden, setHidden] = useState(true);
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  const handleLikeUpdate = async () => {
+    const updatedBlog = {
+      title: blog.title,
+      author: blog.author,
+      url: blog.url,
+      likes: blog.likes + 1,
+      user: blog.user,
+    };
+    await blogService.update(blog.id, updatedBlog);
+    blogService.getAll().then((blogs) => updateBlogs(blogs));
+  };
+
+  if (isHidden) {
+    return (
+      <div>
+        {blog.title} {blog.author}
+        <button onClick={() => setHidden(!isHidden)}>
+          {isHidden ? "view" : "hide"}
+        </button>
+      </div>
+    );
+  } else {
+    return (
+      <div style={blogStyle}>
+        <h3>
+          {blog.title}{" "}
+          <button onClick={() => setHidden(!isHidden)}>
+            {isHidden ? "view" : "hide"}
+          </button>
+        </h3>
+        <p>{blog.author}</p>
+        <p>{blog.url}</p>
+        <p>
+          {blog.likes}
+          <button onClick={() => handleLikeUpdate()}>like</button>
+        </p>
+      </div>
+    );
+  }
+};
+
+export default Blog;
