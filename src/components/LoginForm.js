709199d18dd2be83cc25a943e05581db883cd281
mameli <mameli93@gmail.com>
2020-12-10 23:12:11 +0100

    proptypes and eslint

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,32 +1,33 @@
-import React, { useState } from "react";
-import loginService from "../services/login";
-import noteService from "../services/notes";
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
+import loginService from '../services/login'
+import noteService from '../services/notes'
 
 const LoginForm = ({ loginUser, loginMessage }) => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
 
   const handleLogin = async (event) => {
-    event.preventDefault();
+    event.preventDefault()
 
     try {
       const user = await loginService.login({
         username,
         password,
-      });
+      })
 
-      window.localStorage.setItem("loggedNoteappUser", JSON.stringify(user));
+      window.localStorage.setItem('loggedNoteappUser', JSON.stringify(user))
 
-      noteService.setToken(user.token);
-      setUsername("");
-      setPassword("");
-      loginUser(user);
+      noteService.setToken(user.token)
+      setUsername('')
+      setPassword('')
+      loginUser(user)
     } catch (exception) {
-      loginMessage("wrong credentials");
+      loginMessage('wrong credentials')
     }
 
-    console.log("logging in with", username, password);
-  };
+    console.log('logging in with', username, password)
+  }
 
   return (
     <div>
@@ -51,7 +52,12 @@ const LoginForm = ({ loginUser, loginMessage }) => {
         <button type="submit">login</button>
       </form>
     </div>
-  );
-};
+  )
+}
 
-export default LoginForm;
+LoginForm.propTypes = {
+  loginUser: PropTypes.func.isRequired,
+  loginMessage: PropTypes.func.isRequired,
+}
+
+export default LoginForm
