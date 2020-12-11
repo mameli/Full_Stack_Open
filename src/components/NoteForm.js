31c58ebf98d61970f1c24bb21d041d0039f38fcd
mameli <mameli93@gmail.com>
2020-12-11 23:53:41 +0100

    changed formatting

--- src/components/NoteForm.js
+++ src/components/NoteForm.js
@@ -1,21 +1,21 @@
-import React, { useState } from "react";
+import React, { useState } from 'react'
 
 const NoteForm = ({ createNote }) => {
-  const [newNote, setNewNote] = useState("");
+  const [newNote, setNewNote] = useState('')
 
   const handleChange = (event) => {
-    setNewNote(event.target.value);
-  };
+    setNewNote(event.target.value)
+  }
 
   const addNote = (event) => {
-    event.preventDefault();
+    event.preventDefault()
     createNote({
       content: newNote,
       important: Math.random() > 0.5,
-    });
+    })
 
-    setNewNote("");
-  };
+    setNewNote('')
+  }
 
   return (
     <div>
@@ -26,7 +26,7 @@ const NoteForm = ({ createNote }) => {
         <button type="submit">save</button>
       </form>
     </div>
-  );
-};
+  )
+}
 
-export default NoteForm;
+export default NoteForm
