8f5bcec05ff18550d0735be0db9539c67fff43ec
mameli <mameli93@gmail.com>
2020-12-25 19:16:44 +0100

    redux Provider

--- part6/src/App.js
+++ part6/src/App.js
@@ -1,47 +1,37 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-import { createStore } from 'redux'
-import noteReducer from './reducers/noteReducer'
-
-const store = createStore(noteReducer)
-
-store.dispatch({
-  type: 'NEW_NOTE',
-  data: {
-    content: 'the app state is in redux store',
-    important: true,
-    id: 1
-  }
-})
-
-store.dispatch({
-  type: 'NEW_NOTE',
-  data: {
-    content: 'state changes are made with actions',
-    important: false,
-    id: 2
-  }
-})
+import { createNote, toggleImportanceOf } from './reducers/noteReducer'
+import { useSelector, useDispatch } from 'react-redux'
 
 const App = () => {
-  return(
+  const dispatch = useDispatch()
+  const notes = useSelector((state) => state)
+
+  const addNote = (event) => {
+    event.preventDefault()
+    const content = event.target.note.value
+    event.target.note.value = ''
+    dispatch(createNote(content))
+  }
+
+  const toggleImportance = (id) => {
+    dispatch(toggleImportanceOf(id))
+  }
+
+  return (
     <div>
+      <form onSubmit={addNote}>
+        <input name="note" />
+        <button type="submit">add</button>
+      </form>
       <ul>
-        {store.getState().map(note=>
-          <li key={note.id}>
+        {notes.map((note) => (
+          <li key={note.id} onClick={() => toggleImportance(note.id)}>
             {note.content} <strong>{note.important ? 'important' : ''}</strong>
           </li>
-        )}
-        </ul>
+        ))}
+      </ul>
     </div>
   )
 }
 
-const renderApp = () => {
-  ReactDOM.render(<App />, document.getElementById('root'))
-}
-
-renderApp()
-store.subscribe(renderApp)
-
-export default App;
+export default App
